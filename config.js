43cdc4b6fe7182c6543fbdb68f81496def32f3bd
Enkhbileg <enkhbileg0408@Gmail.com>
2026-05-20 08:29:23 +0800

    M

---
+++ config.js
@@ -0,0 +1,8 @@
+// ICPC Math Contest Configuration
+// Keep this file private - do not share!
+
+const CONFIG = {
+  GEMINI_API_KEY: 'AIzaSyDuRQRanylIysgYZqH8XUOdKpt0RGGvcdw',
+  ADMIN_SECRET: 'admin2025',
+  JUDGE_SECRET: 'judge2025'
+};
